1374add42a2cca96ba287f6b38be078a36c484db
mezitta <michaelezitta@gmail.com>
2023-03-08 14:31:53 -0500

    Text animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,8 @@ import "./Home.css";
 import ProjectLeft from "./ProjectLeft";
 import ProjectRight from "./ProjectRight";
 
+import { motion } from "framer-motion";
+
 const Home = () => {
   return (
     <div className="container-fluid home-container">
@@ -11,16 +13,39 @@ const Home = () => {
         <div className="load-block" />
       </div>
       <div className="row home-header">
-        <div className="col-11">
-          <div className="container-fluid">
-            <div className="row name-row">Michael Zitta</div>
-            <div className="row title-row">Software Developer</div>
+        <div className="col-11 header-left">
+          <div className="container-fluid home-name">
+            <motion.div
+              className="row name-row"
+              initial={{ opacity: 0, x: -8 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ ease: "easeInOut", duration: 0.75, delay: 0.3 }}
+            >
+              Michael Zitta
+            </motion.div>
+            <motion.div
+              className="row title-row"
+              initial={{ opacity: 0, x: -8 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ ease: "easeInOut", duration: 0.75, delay: 0.3 }}
+            >
+              Software Developer
+            </motion.div>
           </div>
         </div>
         <div className="col">Links</div>
       </div>
 
-      <div className="home-body">body</div>
+      <div className="row home-body">
+        <motion.div
+          className="col"
+          initial={{ opacity: 0, x: -8 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ ease: "easeInOut", duration: 0.75, delay: 0.3 }}
+        >
+          body
+        </motion.div>
+      </div>
       <ProjectLeft />
       <ProjectRight />
       <ProjectLeft />
